b3a76a63f4b8412a0d232cf059211afaf07044d8
stevenj <none>
1998-05-07 22:13:51 -0400

    C glue for accessing guile data.

---
+++ base/ctl.c
@@ -0,0 +1,251 @@
+#include "ctl.h"
+
+/**************************************************************************/
+
+/* vector3 utilities: */
+
+vector3 vector3_scale(number s, vector3 v)
+{
+  vector3 vnew;
+
+  vnew.x = s * v.x;
+  vnew.y = s * v.y;
+  vnew.z = s * v.z;
+  return vnew;
+}
+
+vector3 vector3_plus(vector3 v1,vector3 v2)
+{
+  vector3 vnew;
+
+  vnew.x = v1.x + v2.x;
+  vnew.y = v1.y + v2.y;
+  vnew.z = v1.z + v2.z;
+  return vnew;
+}
+
+vector3 vector3_minus(vector3 v1,vector3 v2)
+{
+  vector3 vnew;
+
+  vnew.x = v1.x - v2.x;
+  vnew.y = v1.y - v2.y;
+  vnew.z = v1.z - v2.z;
+  return vnew;
+}
+
+vector3 vector3_cross(vector3 v1,vector3 v2)
+{
+  vector3 vnew;
+
+  vnew.x = v1.y * v2.z - v2.y * v1.z;
+  vnew.y = v1.z * v2.x - v2.z * v1.x;
+  vnew.z = v1.x * v2.y - v2.x * v1.y;
+  return vnew;
+}
+
+/**************************************************************************/
+
+/* variable get/set functions */
+
+/**** Getters ****/
+
+integer ctl_get_integer(char *identifier)
+{
+  return(gh_scm2int(gh_lookup(identifier)));
+}
+
+number ctl_get_number(char *identifier)
+{
+  return(gh_scm2double(gh_lookup(identifier)));
+}
+
+boolean ctl_get_boolean(char *identifier)
+{
+  return(gh_scm2bool(gh_lookup(identifier)));
+}
+
+char* ctl_get_string(char *identifier)
+{
+  return(gh_scm2newstr(gh_lookup(identifier), NULL));
+}
+
+/* This will change to use gh_vector_ref in Guile 1.3: */
+static vector3 scm2vector3(SCM sv)
+{
+  vector3 v;
+
+  v.x = gh_scm2double(gh_vref(sv,gh_int2scm(0)));
+  v.y = gh_scm2double(gh_vref(sv,gh_int2scm(1)));
+  v.z = gh_scm2double(gh_vref(sv,gh_int2scm(2)));
+  return v;
+}
+
+vector3 ctl_get_vector3(char *identifier)
+{
+  return(scm2vector3(gh_lookup(identifier)));
+}
+
+list ctl_get_list(char *identifier)
+{
+  return(gh_lookup(identifier));
+}
+
+object ctl_get_object(char *identifier)
+{
+  return(gh_lookup(identifier));
+}
+
+/**** Setters ****/
+
+static set_value(char *identifier, SCM value)
+{
+  gh_apply(gh_lookup("set!"),
+	   gh_cons(gh_lookup(identifier), 
+		   gh_cons(value, SCM_EOL)));
+}
+
+void ctl_set_integer(char *identifier, integer value)
+{
+  set_value(identifier, gh_int2scm(value));
+}
+
+void ctl_set_number(char *identifier, number value)
+{
+  set_value(identifier, gh_double2scm(value));
+}
+
+void ctl_set_boolean(char *identifier, boolean value)
+{
+  set_value(identifier, gh_bool2scm(value));
+}
+
+void ctl_set_string(char *identifier, char *value)
+{
+  set_value(identifier, gh_str02scm(value));
+}
+
+/* in Guile 1.3, gh_vset will become gh_vector_set */
+void ctl_set_vector3(char *identifier, vector3 value)
+{
+  SCM sv = gh_lookup(identifier);
+
+  gh_vset(sv, gh_int2scm(0), gh_double2scm(value.x));
+  gh_vset(sv, gh_int2scm(1), gh_double2scm(value.y));
+  gh_vset(sv, gh_int2scm(2), gh_double2scm(value.z));
+}
+
+void ctl_set_list(char *identifier, list value)
+{
+  set_value(identifier, value);
+}
+
+void ctl_set_object(char *identifier, object value)
+{
+  set_value(identifier, value);
+}
+
+/**************************************************************************/
+
+/* list traversal */
+
+int list_length(list l)
+{
+  return(gh_list_length(l));
+}
+
+/* Guile 1.2 doesn't have the gh_list_ref function.  Sigh. */
+/* Note: index must be in [0,list_length(l) - 1].  We don't check! */
+static SCM list_ref(list l, int index)
+{
+  SCM cur, rest = l;
+
+  while (index >= 0) {
+    cur = gh_car(rest);
+    rest = gh_cdr(l);
+    --index;
+  }
+  return cur;
+}
+
+integer integer_list_ref(list l, int index)
+{
+  return(gh_scm2int(list_ref(l,index)));
+}
+
+number number_list_ref(list l, int index)
+{
+  return(gh_scm2double(list_ref(l,index)));
+}
+
+boolean boolean_list_ref(list l, int index)
+{
+  return(SCM_BOOL_F != list_ref(l,index));
+}
+
+char* string_list_ref(list l, int index)
+{
+  return(gh_scm2newstr(list_ref(l,index),NULL));
+}
+
+vector3 vector3_list_ref(list l, int index)
+{
+  return(scm2vector3(list_ref(l,index)));
+}
+
+list list_list_ref(list l, int index)
+{
+  return(list_ref(l,index));
+}
+
+object object_list_ref(list l, int index)
+{
+  return(list_ref(l,index));
+}
+
+/**************************************************************************/
+
+/* object properties */
+
+static SCM object_property_value(object o, char *property_name)
+{
+  return(gh_apply(gh_lookup("object-property-value"),
+		  gh_cons(o, 
+			  gh_cons(gh_symbol2scm(property_name),
+				  SCM_EOL))));
+}
+
+integer integer_object_property(object o, char *property_name)
+{
+  return(gh_scm2int(object_property_value(o,property_name)));
+}
+
+number number_object_property(object o, char *property_name)
+{
+  return(gh_scm2double(object_property_value(o,property_name)));
+}
+
+boolean boolean_object_property(object o, char *property_name)
+{
+  return(SCM_BOOL_F != object_property_value(o,property_name));
+}
+
+char* string_object_property(object o, char *property_name)
+{
+  return(gh_scm2newstr(object_property_value(o,property_name),NULL));
+}
+
+vector3 vector3_object_property(object o, char *property_name)
+{
+  return(scm2vector3(object_property_value(o,property_name)));
+}
+
+list list_object_property(object o, char *property_name)
+{
+  return(object_property_value(o,property_name));
+}
+
+object object_object_property(object o, char *property_name)
+{
+  return(object_property_value(o,property_name));
+}
---
+++ base/ctl.h
@@ -0,0 +1,89 @@
+#ifndef CTL_H
+#define CTL_H
+
+#include <guile/gh.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif                          /* __cplusplus */
+
+/**************************************************************************/
+
+  /* Basic types: */
+
+typedef int integer;
+typedef double number;
+typedef short boolean;
+typedef SCM list;
+typedef SCM object;
+
+  /* define vector3 as a structure, not an array, so that it can
+     be a function return value and so that simple assignment works. */
+typedef struct {
+  number x,y,z;
+} vector3;
+
+/**************************************************************************/
+
+  /* vector3 utilities: */
+
+#define vector3_dot(v1,v2) (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z)
+#define vector3_norm(v) sqrt(vector3_dot(v,v))
+
+extern vector3 vector3_scale(number s, vector3 v);
+extern vector3 vector3_cross(vector3 v1,vector3 v2);
+extern vector3 vector3_plus(vector3 v1,vector3 v2);
+extern vector3 vector3_minus(vector3 v1,vector3 v2);
+
+/**************************************************************************/
+
+  /* variable get/set functions */
+
+extern integer ctl_get_integer(char *identifier);
+extern number ctl_get_number(char *identifier);
+extern boolean ctl_get_boolean(char *identifier);
+extern char* ctl_get_string(char *identifier);
+extern vector3 ctl_get_vector3(char *identifier);
+extern list ctl_get_list(char *identifier);
+extern object ctl_get_object(char *identifier);
+
+extern void ctl_set_integer(char *identifier, integer value);
+extern void ctl_set_number(char *identifier, number value);
+extern void ctl_set_boolean(char *identifier, boolean value);
+extern void ctl_set_string(char *identifier, char *value);
+extern void ctl_set_vector3(char *identifier, vector3 value);
+extern void ctl_set_list(char *identifier, list value);
+extern void ctl_set_object(char *identifier, object value);
+
+/**************************************************************************/
+
+  /* list traversal */
+
+extern int list_length(list l);
+extern integer integer_list_ref(list l, int index);
+extern number number_list_ref(list l, int index);
+extern boolean boolean_list_ref(list l, int index);
+extern char* string_list_ref(list l, int index);
+extern vector3 vector3_list_ref(list l, int index);
+extern list list_list_ref(list l, int index);
+extern object object_list_ref(list l, int index);
+
+/**************************************************************************/
+
+  /* object properties */
+
+extern integer integer_object_property(object o, char *property_name);
+extern number number_object_property(object o, char *property_name);
+extern boolean boolean_object_property(object o, char *property_name);
+extern char* string_object_property(object o, char *property_name);
+extern vector3 vector3_object_property(object o, char *property_name);
+extern list list_object_property(object o, char *property_name);
+extern object object_object_property(object o, char *property_name);
+
+/**************************************************************************/
+
+#ifdef __cplusplus
+	   }                               /* extern "C" */
+#endif                          /* __cplusplus */
+
+#endif /* CTL_H */
