040bfbedf519e716a359af1ad11bd16d50da5bd8
stevenj <stevenj@fftw.org>
2005-06-05 22:31:03 -0400

    in libgeom, set default values for input variables (geometry_lattice, etcetera)

---
+++ utils/geom-ctl-io-defaults.c
@@ -0,0 +1,20 @@
+/* for inclusion into geom-ctl-io.c ... this is somewhat of a hack,
+ necessitated because gen-ctl-io doesn't write default values (from
+ geom.scm) for the input variable, instead relying on them being
+ initialized from Scheme. */
+
+integer dimensions = 3;
+material_type default_material = { 0 };
+vector3 geometry_center = { 0, 0, 0 };
+lattice geometry_lattice = { { 1,0,0 },
+			     { 0,1,0 },
+			     { 0,0,1 },
+			     { 1e20,1e20,1e20 },
+			     { 1,1,1 },
+			     { 1,0,0 },
+			     { 0,1,0 },
+			     { 0,0,1 },
+			     { { 1,0,0 }, { 0,1,0 }, { 0,0,1 } },
+			     { { 1,0,0 }, { 0,1,0 }, { 0,0,1 } } };
+geometric_object_list geometry = { 0, 0 };
+boolean ensure_periodicity = 0;
