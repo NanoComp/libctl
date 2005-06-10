31fab9c86e2c08b58bef33b04047ad2a76df3d85
stevenj <stevenj@fftw.org>
2005-06-10 16:38:04 -0400

    tweak

--- src/integrator.c
+++ src/integrator.c
@@ -195,7 +195,7 @@ static unsigned ls0(unsigned n)
 #else
 static unsigned ls0(unsigned n)
 {
-     const unsigned bits[] = {
+     const unsigned bits[256] = {
 	  0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4,
 	  0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 5,
 	  0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4,
