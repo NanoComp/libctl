dad4303ab3e38565bbbdde36641ae90e882352df
stevenj <stevenj@fftw.org>
2005-06-11 18:24:24 -0400

    added more test functions from gsl-1.6/monte/test.c

--- src/integrator.c
+++ src/integrator.c
@@ -742,6 +742,71 @@ int count = 0;
 int which_integrand = 0;
 const double radius = 0.50124145262344534123412; /* random */
 
+/* Simple constant function */
+double
+fconst (double x[], size_t dim, void *params)
+{
+  return 1;
+}
+
+/*** f0, f1, f2, and f3 are test functions from the Monte-Carlo
+     integration routines in GSL 1.6 (monte/test.c).  Copyright (c)
+     1996-2000 Michael Booth, GNU GPL. ****/
+
+/* Simple product function */
+double f0 (unsigned dim, const double *x, void *params)
+{
+     double prod = 1.0;
+     unsigned int i;
+     for (i = 0; i < dim; ++i)
+	  prod *= 2.0 * x[i];
+     return prod;
+}
+
+/* Gaussian centered at 1/2. */
+double f1 (unsigned dim, const double *x, void *params)
+{
+     double a = *(double *)params;
+     double sum = 0.;
+     unsigned int i;
+     for (i = 0; i < dim; i++) {
+	  double dx = x[i] - 0.5;
+	  sum += dx * dx;
+     }
+     return (pow (M_2_SQRTPI / (2. * a), (double) dim) *
+	     exp (-sum / (a * a)));
+}
+
+/* double gaussian */
+double f2 (unsigned dim, const double *x, void *params)
+{
+     double a = *(double *)params;
+     double sum1 = 0.;
+     double sum2 = 0.;
+     unsigned int i;
+     for (i = 0; i < dim; i++) {
+	  double dx1 = x[i] - 1. / 3.;
+	  double dx2 = x[i] - 2. / 3.;
+	  sum1 += dx1 * dx1;
+	  sum2 += dx2 * dx2;
+     }
+     return 0.5 * pow (M_2_SQRTPI / (2. * a), dim) 
+	  * (exp (-sum1 / (a * a)) + exp (-sum2 / (a * a)));
+}
+
+/* Tsuda's example */
+double f3 (unsigned dim, const double *x, void *params)
+{
+     double c = *(double *)params;
+     double prod = 1.;
+     unsigned int i;
+     for (i = 0; i < dim; i++)
+	  prod *= c / (c + 1) * pow((c + 1) / (c + x[i]), 2.0);
+     return prod;
+}
+
+/*** end of GSL test functions ***/
+
 double f_test(unsigned dim, const double *x, void *data)
 {
      double val;
@@ -763,14 +828,25 @@ double f_test(unsigned dim, const double *x, void *data)
 	      }
 	      val = exp(-val) * scale;
 	      break;
+	 }
 	 case 2: /* discontinuous objective: volume of hypersphere */
 	      val = 0;
 	      for (i = 0; i < dim; ++i)
 		   val += x[i] * x[i];
 	      val = val < radius * radius;
 	      break;
-	 }
-		   
+	 case 3:
+	      val = f0(dim, x, data);
+	      break;
+	 case 4:
+	      val = f1(dim, x, data);
+	      break;
+	 case 5:
+	      val = f2(dim, x, data);
+	      break;
+	 case 6:
+	      val = f3(dim, x, data);
+	      break;
 	 default:
 	      fprintf(stderr, "unknown integrand %d\n", which_integrand);
 	      exit(EXIT_FAILURE);
@@ -807,15 +883,11 @@ static double exact_integral(unsigned dim, const double *xmax) {
 	      for (i = 0; i < dim; ++i)
 		   val *= sin(xmax[i]);
 	      break;
-	 case 1:
-	      val = 1;
-	      break;
 	 case 2:
 	      val = dim == 0 ? 1 : S(dim) * pow(radius * 0.5, dim) / dim;
 	      break;
 	 default:
-	      fprintf(stderr, "unknown integrand %d\n", which_integrand);
-              exit(EXIT_FAILURE);
+	      val = 1.0;
      }
      return val;
 }
@@ -825,22 +897,27 @@ int main(int argc, char **argv)
      double *xmin, *xmax;
      double tol, val, err;
      unsigned i, dim, maxEval;
+     double fdata;
 
      dim = argc > 1 ? atoi(argv[1]) : 2;
      tol = argc > 2 ? atof(argv[2]) : 1e-2;
      which_integrand = argc > 3 ? atoi(argv[3]) : 0;
      maxEval = argc > 4 ? atoi(argv[4]) : 0;
 
+     fdata = which_integrand == 6 ? (1.0 + sqrt (10.0)) / 9.0 : 0.1;
+
      xmin = (double *) malloc(dim * sizeof(double));
      xmax = (double *) malloc(dim * sizeof(double));
      for (i = 0; i < dim; ++i) {
 	  xmin[i] = 0;
-	  xmax[i] = 1 + (which_integrand == 2 ? 0 : 0.4 * sin(i));
+	  xmax[i] = 1 + (which_integrand >= 2 ? 0 : 0.4 * sin(i));
      }
 
      printf("%u-dim integral, tolerance = %g, integrand = %d\n",
 	    dim, tol, which_integrand);
-     adapt_integrate(f_test, 0, dim, xmin, xmax, maxEval, 0, tol, &val, &err);
+     adapt_integrate(f_test, &fdata, 
+		     dim, xmin, xmax, 
+		     maxEval, 0, tol, &val, &err);
      printf("integration val = %g, est. err = %g, true err = %g\n", 
 	    val, err, fabs(val - exact_integral(dim, xmax)));
      printf("#evals = %d\n", count);
