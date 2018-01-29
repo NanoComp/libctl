8ef00d75ce97dc7bbb6e9b720c7c3ec8e8d25be6
Steven G. Johnson <stevenj@mit.edu>
2018-01-29 12:34:38 -0500

    fix #11

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p m4
+
 # paranoia: sometimes autoconf doesn't get things right the first time
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
